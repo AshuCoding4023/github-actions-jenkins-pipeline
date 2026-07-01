4d0d2a81b4c466d19b0e01416c6daf7dccc70a8b
unknown <rashish199@gmail.com>
2026-07-01 15:02:13 +0530

    Initial Java Project

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println(" GitHub Actions CI/CD Demo ");
+        System.out.println(" Hello World ");
+        System.out.println("==================================");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
